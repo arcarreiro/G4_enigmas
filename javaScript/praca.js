9b9f9d8ca52746f1f02e854670ac2daef084c8b3
arcarreiro <arcarreiro@gmail.com>
2024-09-30 19:21:53 -0300

    Fix: Cabeçalho da praça

--- javaScript/praca.js
+++ javaScript/praca.js
@@ -10,6 +10,25 @@ let imagemFixa = '../assets/praca.png';
 
 let fundoTela = document.getElementById('praca');
 
+window.addEventListener('load', function () {
+    const imagem = document.getElementById('praca');
+    const rodape = document.getElementById('opcoes-praca');
+    const header = document.getElementById('cabecalho');
+
+    function ajustarFooter() {
+        let largura = imagem.offsetWidth;
+        rodape.style.width = largura + "px";
+        header.style.width = largura + "px";
+    }
+
+    if (imagem.complete) {
+        ajustarFooter();
+    }
+
+    imagem.addEventListener('load', ajustarFooter);
+
+    window.addEventListener('resize', ajustarFooter);
+});
 
 // highlight do arquivo
 buttonArvores.addEventListener('mouseover', () => {
@@ -36,25 +55,6 @@ button.addEventListener('mouseout', () => {
 
 
 
-window.addEventListener('load', function () {
-    const imagem = document.getElementById('praca');
-    const rodape = document.getElementById('opcoes-praca');
-    const header = document.getElementById('cabecalho');
-
-    function ajustarFooter() {
-        let largura = imagem.offsetWidth;
-        rodape.pracastyle.width = largura + "px";
-        header.pracastyle.width = largura + "px";
-    }
-
-    if (imagem.complete) {
-        ajustarFooter();
-    }
-
-    imagem.addEventListener('load', ajustarFooter);
-
-    window.addEventListener('resize', ajustarFooter);
-});
 
 
 //modal
